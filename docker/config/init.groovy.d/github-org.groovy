76d8dd3431ee4652750cf0c6abbb9fbfc4f0e642
Stephen Badger <stephen.badger@gmail.com>
2020-03-17 20:10:38 +0000

    Set up correct traits for pull requests

--- docker/config/init.groovy.d/github-org.groovy
+++ docker/config/init.groovy.d/github-org.groovy
@@ -9,6 +9,13 @@ import org.jenkinsci.plugins.github_branch_source.*
 def folder = Jenkins.instance.items.isEmpty() ? Jenkins.instance.createProject(OrganizationFolder, 'BYOND') : Jenkins.instance.items[0]
 def navigator = new GitHubSCMNavigator('BYOND')
 navigator.credentialsId = 'github-org'
+navigator.traits = [
+  new jenkins.scm.impl.trait.WildcardSCMSourceFilterTrait('*', ''),
+  new jenkins.scm.impl.trait.RegexSCMHeadFilterTrait('.*'),
+  new BranchDiscoveryTrait(1), // Exclude branches that are also filed as PRs.
+  new OriginPullRequestDiscoveryTrait(1), // Merging the pull request with the current target branch revision.
+  new ForkPullRequestDiscoveryTrait(1, new ForkPullRequestDiscoveryTrait.TrustPermission()),  // Allow people in the organisation to update Jenkinsfiles in forks
+]
 folder.navigators.replace(navigator)
 Jenkins.instance.save()
 navigator.afterSave(folder)
